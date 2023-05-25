ca117c4b779d4844d7918c6496a668f39cc0624d
Karmen Too <karmen.tanaka@gmail.com>
2023-05-25 15:08:19 -0400

    done

--- bubble.js
+++ bubble.js
@@ -1,3 +1,27 @@
-function bubbleSort() {}
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let sorted = false
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                sorted = true
+            } else if (arr[j] == arr[j + 1]) {
+                sorted = true
+            }
+
+
+        }
+        if (!sorted) {
+            break;
+        }
+
+    }
+    console.log(arr)
+    return arr
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,40 @@
-function merge() {}
+function merge(arr1, arr2) {
+    //if both lengths are still more than 0
+    //compare first value of one array to the next value and take the smallest value to put it in the results array
+    let results = []
+    let i1 = 0
+    let j2 = 0
+    while (i1 < arr1.length && j2 < arr2.length) {
+        if (arr1[i1] <= arr2[j2]) {
+            results.push(arr1[i1])
+            i1++
+        } else {
+            results.push(arr2[j2])
+            j2++
+        }
+    }
+    //if one of the array is done, just add the end of the other array to the end
+    while (i1 < arr1.length) {
+        results.push(arr1[i1])
+        i1++
+    }
+    while (j2 < arr2.length) {
+        results.push(arr2[j2])
+        j2++
+    }
+    return results
+}
 
-function mergeSort() {}
+function mergeSort(arr) {
+    //check if array length is <=1
+    if (arr.length <= 1) return arr
 
-module.exports = { merge, mergeSort};
+    //if array is larger than 1, break array down into 2 arrays
+    let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    //if they are, then merge the left and right arrays and repeat for all of them
+    return merge(left, right)
+}
+
+module.exports = { merge, mergeSort };
